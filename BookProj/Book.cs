ab082685e45deeb43a4944143447b66b3d128d86
YKShtykov <yakov.shtykov@gmail.com>
2016-07-13 00:40:21 +0300

    Initial commit

---
+++ BookProj/Book.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookProj
+{
+  public class Book : IEquatable<Book>, IComparable<Book>
+  {
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public int PageCount { get; set; }
+    public int Cost { get; set; }
+
+    public Book(string author = "Leo Tolstoy", string title = "War and peace", int pageCount = 1274, int cost = 40)
+    {
+      Author = author;
+      Title = title;
+      PageCount = pageCount;
+      Cost = cost;
+    }
+
+    public int CompareTo(Book other)
+    {
+      return Cost.CompareTo(other.Cost);
+    }
+
+    public int CompareTo(Book other, IComparer<Book> comparer)
+    {
+      return comparer.Compare(this, other);
+    }
+
+    public bool Equals(Book other)
+    {
+      if (ReferenceEquals(null, other)) return false;
+      if (ReferenceEquals(this, other)) return true;
+
+      return (Author == other.Author && Title == other.Title && PageCount == other.PageCount);
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+
+      if (obj.GetType() == typeof(Book)) return Equals((Book)obj);
+      return false;
+    }
+
+    public override int GetHashCode()
+    {
+      return 555 * Author.GetHashCode() + 55 * Title.GetHashCode() + 5 * PageCount + Cost;
+    }
+
+    public override string ToString()
+    {
+      return string.Format("Book: {0}; Author: {1}; {2} pages; Cost {3:c}", Title, Author, PageCount, Cost);
+    }
+  }
+
+  public class AuthorComparer : IComparer<Book>
+  {
+    public int Compare(Book x, Book y)
+    {
+      return x.Author.CompareTo(y.Author);
+    }
+  }
+
+  public class TitleComparer : IComparer<Book>
+  {
+    public int Compare(Book x, Book y)
+    {
+      return x.Title.CompareTo(y.Title);
+    }
+  }
+
+  public class PageCountComparer : IComparer<Book>
+  {
+    public int Compare(Book x, Book y)
+    {
+      return x.PageCount.CompareTo(y.PageCount);
+    }
+  }
+}
---
+++ BookProjTests/BookTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookProj;
+using System.Diagnostics;
+
+namespace BookProjTests
+{
+  [TestClass]
+  public class BookTests
+  {
+    public Book[] arr;
+    public Book[] sortedByCostArr;
+    public Book[] sortedByTitleArr;
+    public Book[] sortedByPageCountArr;
+
+    [TestInitialize]
+    public void Init()
+    {
+      Book warAndPeace = new Book();
+      Book goldenBook = new Book("Midas", "Golden Book", 500, 1000000);
+      arr = new Book[] { goldenBook, warAndPeace };
+      sortedByTitleArr = new Book[] { goldenBook, warAndPeace };
+      sortedByCostArr = new Book[] { warAndPeace, goldenBook };
+      sortedByPageCountArr = new Book[] { goldenBook, warAndPeace };
+    }
+
+    [TestMethod]
+    public void InterfaceSort()
+    {
+      //Arrange 
+      foreach (var item in arr)
+      {
+        Debug.WriteLine(item.ToString());
+      }
+      //Act
+      Array.Sort(arr);
+
+      foreach (var item in arr)
+      {
+        Debug.WriteLine(item.ToString());
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(arr, sortedByCostArr);
+    }
+
+    [TestMethod]
+    public void SortTest()
+    {
+      //Arrange 
+      PageCountComparer comparer = new PageCountComparer();
+      foreach (var item in arr)
+      {
+        Debug.WriteLine(item.ToString());
+      }
+      //Act
+      Array.Sort(arr, comparer);
+
+      foreach (var item in arr)
+      {
+        Debug.WriteLine(item.ToString());
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(arr, sortedByPageCountArr);
+    }
+
+    [TestMethod]
+    public void TitleSortTest()
+    {
+      //Arrange 
+      TitleComparer comparer = new TitleComparer();
+      foreach (var item in arr)
+      {
+        Debug.WriteLine(item.ToString());
+      }
+      //Act
+      Array.Sort(arr, comparer);
+
+      foreach (var item in arr)
+      {
+        Debug.WriteLine(item.ToString());
+      }
+
+      //Assert
+      CollectionAssert.AreEqual(arr, sortedByTitleArr);
+    }
+  }
+}
+
+
+
+
